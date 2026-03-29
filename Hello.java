a5ccb29dbaf7f22e2fd116acc6b5106af65b318d
Krishan098 <krishanmittal018rocker@gmail.com>
2026-03-29 23:15:01 +0530

    Hello

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// The compiler(javac) and the launcher(java) are both case-sensitive
